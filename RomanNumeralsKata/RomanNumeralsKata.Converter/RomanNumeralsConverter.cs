8c9df092195484aef7e5af3c13f5309f284e36b9
cm-rogers <49442265+cm-rogers@users.noreply.github.com>
2019-04-29 01:32:56 +0100

    Refactored to use a list of numerals that returns a string using linq.aggregate.

--- RomanNumeralsKata/RomanNumeralsKata.Converter/RomanNumeralsConverter.cs
+++ RomanNumeralsKata/RomanNumeralsKata.Converter/RomanNumeralsConverter.cs
@@ -1,20 +1,38 @@
-﻿namespace RomanNumeralsKata
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumeralsKata
 {
+    internal class RomanNumeral
+    {
+        public int DecimalValue = 0;
+        public string NumeralValue = "";
+    }
+
     public class RomanNumeralsConverter
     {
+        private readonly IEnumerable<RomanNumeral> _numerals = new List<RomanNumeral>
+        {
+            new RomanNumeral{DecimalValue = 1, NumeralValue = "I"},
+        };
+
         public string Convert(int decimalValue)
         {
-            if (decimalValue == 1)
-            {
-                return "I";
-            }
+            return _numerals
+                .Aggregate(
+                    new RomanNumeral(),
+                    (result, numeral) =>
+                    {
+                        // @TODO: Remove this use of `while`
+                        while (result.DecimalValue < decimalValue)
+                        {
+                            result.DecimalValue += numeral.DecimalValue;
+                            result.NumeralValue += numeral.NumeralValue;
+                        }
 
-            if (decimalValue == 2)
-            {
-                return "II";
-            }
-
-            return "";
+                        return result;
+                    })
+                .NumeralValue;
         }
     }
 }
